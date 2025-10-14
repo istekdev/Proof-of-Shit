c550f579eea342d4ba8a610f89cddc0d3c5859ff
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-10-14 08:31:36 -0500

    Create poshit.py

---
+++ poshit.py
@@ -0,0 +1,34 @@
+from termcolor import colored
+from hashlib import sha256
+import time
+import os
+
+difficulty = 10000 * 60
+target = (2**224) * ((2**32) - 1) // difficulty
+
+def poshit():
+  max = (2**32) - 1
+  nonce = 0
+  
+  version = 1
+  height = 1
+  prevHash = sha256(sha256(os.urandom(1)).digest()).digest()
+  merkle = sha256(sha256(os.urandom(1)).digest()).digest()
+  targethex = target.hex()
+  timestamp = str(time.time())
+  while nonce <= max:
+    blockHash = sha256(sha256(nonce.to_bytes(32, "big") + height.to_bytes(4, "big") + bytes.fromhex(targethex) + version.to_bytes(4, "big") + prevHash + merkle + timestamp.encode("utf-8")).digest()).hexdigest()
+    print(colored(f"Mining - Current Hash: {blockHash}", "yellow", attrs=["bold"]))
+    if os.path.exists("hashes.txt"):
+      with open("hashes.txt", "a") as a:
+        a.write(blockHash + "," + "\n")
+    else:
+      with open("hashes.txt", "w") as w:
+        w.write(blockHash + "," + "\n")
+    if int(blockHash, 16):
+      print(colored("Block Has Been Mined!", "green", attrs=["bold"]))
+      break
+    nonce += 1
+
+poshit()
+  
