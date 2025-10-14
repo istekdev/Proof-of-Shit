d03e3be5f928b8f156234f903f27091110244044
ISTEK <200693214+istekdev@users.noreply.github.com>
2025-10-14 08:55:09 -0500

    Update poshit.py

--- poshit.py
+++ poshit.py
@@ -17,7 +17,7 @@ def poshit():
   targethex = target.hex()
   timestamp = str(time.time())
   while nonce <= max:
-    blockHash = sha256(sha256(nonce.to_bytes(32, "big") + height.to_bytes(4, "big") + bytes.fromhex(targethex) + version.to_bytes(4, "big") + prevHash + merkle + timestamp.encode("utf-8")).digest()).hexdigest()
+    blockHash = sha256(sha256(nonce.to_bytes(32, "big") + height.to_bytes(4, "big") + target.to_bytes(32, "big") + version.to_bytes(4, "big") + prevHash + merkle + timestamp.encode("utf-8")).digest()).hexdigest()
     print(colored(f"Mining - Current Hash: {blockHash}", "yellow", attrs=["bold"]))
     if os.path.exists("hashes.txt"):
       with open("hashes.txt", "a") as a:
@@ -25,7 +25,7 @@ def poshit():
     else:
       with open("hashes.txt", "w") as w:
         w.write(blockHash + "," + "\n")
-    if int(blockHash, 16):
+    if int(blockHash, 16) <= target:
       print(colored("Block Has Been Mined!", "green", attrs=["bold"]))
       break
     nonce += 1
